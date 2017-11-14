85840ae62db76c5c91696d6b28832ff531834f57
Ayoub Lesfer <alesfer@emi.u-bordeaux1.fr>
2017-11-14 18:32:55 +0100

    Projet

--- Projet2017/src/mapio.c
+++ Projet2017/src/mapio.c
@@ -21,10 +21,11 @@ void map_new (unsigned width, unsigned height)
     map_set (width - 1, y, 1); // Wall
   }
 
-  map_object_begin (6);
+  map_object_begin (8);
 
   // Texture pour le sol
   map_object_add ("images/ground.png", 1, MAP_OBJECT_SOLID);
+
   // Mur
   map_object_add ("images/wall.png", 1, MAP_OBJECT_SOLID);
   // Gazon
@@ -35,14 +36,38 @@ void map_new (unsigned width, unsigned height)
   map_object_add ("images/herb.png", 1, MAP_OBJECT_AIR);
   // Petit plancher flottant
   map_object_add ("images/floor.png", 1, MAP_OBJECT_SEMI_SOLID);
+  // Fleur
+  map_object_add ("images/flower.png", 1, MAP_OBJECT_AIR);
+  // Coin
+  map_object_add ("images/coin.png", 20, MAP_OBJECT_AIR | MAP_OBJECT_COLLECTIBLE);
+
+
 
   map_object_end ();
 }
 
 void map_save (char *filename)
 {
-  // TODO
-  fprintf (stderr, "Sorry: Map save is not yet implemented\n");
+  int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+  if(fd == -1){
+    fprintf (stderr, "Error 69: Save failed\n");
+  }
+
+  int map_width = map_width();
+  int map_height = map_height();
+  int map_objects = map_objects();
+
+  int objects[map_width*map_height];
+  for(int x=0; x<width; x++){
+    for(int y=0; y<height; y++){
+      objects[x+map_width*y];
+    }
+  }
+
+  char buf[100] = "Map Width: ";
+  write(fd, &buf, 11*sizeof(char));
+
+  close(fd);
 }
 
 void map_load (char *filename)
