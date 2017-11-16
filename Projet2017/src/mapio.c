f116dbf70aca9bd4a8ff82543f967b06aaf3e5e8
Ayoub Lesfer <alesfer@emi.u-bordeaux1.fr>
2017-11-16 14:11:58 +0100

    Projet Part1 - Fini

--- Projet2017/src/mapio.c
+++ Projet2017/src/mapio.c
@@ -50,30 +50,115 @@ void map_save (char *filename)
 {
   int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
   if(fd == -1){
-    fprintf (stderr, "Error 69: Save failed\n");
+    fprintf (stderr, "Error 001: Save Failed\n");
   }
 
-  int map_width = map_width();
-  int map_height = map_height();
-  int map_objects = map_objects();
+  int width = map_width();
+  write(fd, &width, sizeof(int));
+  int height = map_height();
+  write(fd, &height, sizeof(int));
+  int nb_objects = map_objects();
+  write(fd, &nb_objects, sizeof(int));
 
-  int objects[map_width*map_height];
+  int map[width*height];
   for(int x=0; x<width; x++){
     for(int y=0; y<height; y++){
-      objects[x+map_width*y];
+      map[x+width*y] = map_get(x,y);
+      write(fd, map+(x+width*y), sizeof(int));
     }
   }
 
-  char buf[100] = "Map Width: ";
-  write(fd, &buf, 11*sizeof(char));
+  for(int i=0; i<nb_objects; i++){
+    char *object = map_get_name(i);
+    char *buf = calloc(256, sizeof(char));
+    strcpy(buf, object);
+    write(fd, buf, 256*sizeof(char));
+    int frames = map_get_frames(i);
+    write(fd, &frames, sizeof(int));
+    int solidity = map_get_solidity(i);
+    write(fd, &solidity, sizeof(int));
+    int destructible = map_is_destructible(i);
+    write(fd, &destructible, sizeof(int));
+    int collectible = map_is_collectible(i);
+    write(fd, &collectible, sizeof(int));
+    int generator = map_is_generator(i);
+    write(fd, &generator, sizeof(int));
+  }
 
   close(fd);
 }
 
 void map_load (char *filename)
 {
-  // TODO
-  exit_with_error ("Map load is not yet implemented\n");
+  int fd = open(filename, O_RDONLY);
+  if(fd == -1){
+    fprintf (stderr, "Error 002: Load Failed\n");
+  }
+
+  int width;
+  read(fd, &width, sizeof(int));
+  int height;
+  read(fd, &height, sizeof(int));
+
+  map_allocate (width, height);
+
+  int nb_objects;
+  read(fd, &nb_objects, sizeof(int));
+
+  int objects[width*height];
+  for(int x=0; x<width; x++){
+    for(int y=0; y<height; y++){
+      read(fd, objects+(x+width*y), sizeof(int));
+    }
+  }
+
+  map_object_begin (nb_objects);
+
+  for(int i=0; i<nb_objects; i++){
+    char *object = calloc(256, sizeof(char));
+    char buf = ' ';
+    int count=0;
+    while(buf != 0){
+      read(fd, &buf, sizeof(char));
+      count++;
+    }
+    lseek(fd, (0-count) * sizeof(char), SEEK_CUR);
+    read(fd, object, count*sizeof(char));
+    lseek(fd, (256-count) * sizeof(char), SEEK_CUR);
+
+    int frames;
+    read(fd, &frames, sizeof(int));
+
+    int obj_type;
+    int solidity;
+    read(fd, &solidity, sizeof(int));
+    obj_type = solidity;
+    int destructible;
+    read(fd, &destructible, sizeof(int));
+    if(destructible){
+      obj_type = obj_type | MAP_OBJECT_DESTRUCTIBLE;
+    }
+    int collectible;
+    read(fd, &collectible, sizeof(int));
+    if(collectible){
+      obj_type = obj_type | MAP_OBJECT_COLLECTIBLE;
+    }
+    int generator;
+    read(fd, &generator, sizeof(int));
+    if(generator){
+      obj_type = obj_type | MAP_OBJECT_GENERATOR;
+    }
+    map_object_add (object, frames, obj_type);
+  }
+
+  for(int x=0; x<width; x++){
+    for(int y=0; y<height; y++){
+      map_set(x, y, objects[x+width*y]);
+    }
+  }
+
+  map_object_end ();
+  close(fd);
 }
 
 #endif
